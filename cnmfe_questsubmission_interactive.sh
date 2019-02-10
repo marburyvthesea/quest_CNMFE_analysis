a47e56ca647d50a98ddaaf91108df4e909b88261
marburyvthesea <jjm2128@gmail.com>
2019-02-10 15:19:48 -0600

    changes

---
+++ cnmfe_questsubmission_interactive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~
+
+#add project directory to PATH
+export PATH=$PATH/projects/p30771/
+
+#load modules to use
+module load python/anaconda3.6 
+
+#need to cd to load conda environment
+
+cd pythonenvs
+source activate miniscoPy
+
+#need to cd to module directory
+
+cd ..
+cd /home/jma819/miniscope/analysis_code/miniscoPy/jjm_module
+
+#run normcorr
+
+
+python main_cnmfe_e_jjm_script.py /projects/p30771/miniscope/data/GRIN009/1_23_2019/motion_corrected_cnmfe.hdf5
