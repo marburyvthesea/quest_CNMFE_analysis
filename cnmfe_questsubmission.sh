ed28ea8ddff7026a5b6d393e006584b7f3ba52e8
chief <chief>
2019-02-09 23:35:53 -0600

    changes to cnmfe submission

--- cnmfe_questsubmission.sh
+++ cnmfe_questsubmission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #MSUB -A p30771
-#MSUB -q short
-#MSUB -l walltime=04:00:60
+#MSUB -q normal
+#MSUB -l walltime=03:59:00
 #MSUB -M johnjmarshall@u.northwestern.edu
 #MSUB -j oe
 #MSUB -N CNMFE
