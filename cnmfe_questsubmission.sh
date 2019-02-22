d204f34cae6a44d03595df81a63cff9582e5c915
marburyvthesea <jjm2128@gmail.com>
2019-02-22 15:17:40 -0600

    changes

--- cnmfe_questsubmission.sh
+++ cnmfe_questsubmission.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #MSUB -A p30771
 #MSUB -q short
-#MSUB -l walltime=4:00:00
+#MSUB -l walltime=1:00:00
 #MSUB -M johnjmarshall@u.northwestern.edu
 #MSUB -j oe
 #MSUB -N CNMFE
-#MSUB -l nodes=1:ppn=20
+#MSUB -l nodes=1:ppn=12
 cd ~
 
 #add project directory to PATH
@@ -27,4 +27,4 @@ cd /home/jma819/miniscope/analysis_code/miniscoPy/jjm_module
 #run normcorr
 
 
-python main_cnmfe_e_jjm_script.py /projects/p30771/miniscope/data/GRIN011/1_24_2019/H10_M19_S59 motion_corrected.hdf5
+python main_cnmfe_e_jjm_script.py /projects/p30771/miniscope/data/GRIN011/1_24_2019/H10_M19_S59/mmmap_files H10_M19_S59msCam9_rig__d1_480_d2_752_d3_1_order_F_frames_1000_2d_forquest_cnmfe.hdf5
